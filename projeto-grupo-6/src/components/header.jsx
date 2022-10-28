95848b0ab2a967ac86b1ba071c1807cec8ba37de
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-27 23:04:48 -0300

    landing page feita com o grupo no exercicio em aula

---
+++ projeto-grupo-6/src/components/header.jsx
@@ -0,0 +1,58 @@
+
+import LOGO from '../assets/logo.png'
+
+import wpp from '../assets/Hero.png'
+
+import './header.css'
+
+
+
+function header() {
+
+    return (
+        <header>
+            <nav>
+            <a href="http://localhost:5173/" className='logo'>
+                <img src={LOGO} alt="logo The box" />
+                <h3>The box</h3>
+            </a>
+
+            <ul>
+                <li>
+                    <a href="">
+                     Home
+                    </a>  
+                </li>
+                <li>
+                    <a href="">
+                    About us
+                    </a>    
+                </li>
+                <li>
+                    <a href="">
+                    Projects
+                    </a>    
+                </li>
+                <li>
+                    <a href="">
+                    Services
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                    Contact us
+                    </a>
+                </li>
+            </ul>
+            </nav>
+
+
+        <img src={wpp} alt="" className='mainHeader'/>
+
+        </header>
+    );
+
+}
+
+
+export default header;
